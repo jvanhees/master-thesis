338488f681ec65b48c564a72f4c45f493c99b075
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-03-07 17:20:31 +0100

    First test with OpenCV

---
+++ app/script.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+#out = cv2.VideoWriter('output.avi',fourcc, fps, (int(width),int(height)))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.resize(frame, (int(width / 2), int(height / 2)))
+        
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        gray = np.float32(gray)
+        dst = cv2.cornerHarris(gray,2,3,0.04)
+        
+        #result is dilated for marking the corners, not important
+        dst = cv2.dilate(dst,None)
+
+        # Threshold for an optimal value, it may vary depending on the image.
+        frame[dst>0.01*dst.max()]=[0,0,255]        
+        
+        
+        # show the frame
+        cv2.imshow('Result',frame)
+        
+        #out.write(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+#out.release()
+cv2.destroyAllWindows()
