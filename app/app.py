e792d7b4897eea6c3df27a4e27b7ba588d9878d6
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-03-31 15:43:41 +0200

    App structure

---
+++ app/app.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import timeit
+
+import helpers.caffenet as caffenet
+
+labels = caffenet.getLabels()
+result = caffenet.classify('data/cat-wallpaper-38.jpg');
+
+# sort top five predictions from softmax output
+top_inds = result.argsort()[::-1][:5]  # reverse sort and take five largest items
+print 'probabilities and labels:'
+print zip(result[top_inds], labels[top_inds])
+
+# print 'output label:', labels[output_prob.argmax()]
+
+# Timer
+# print timeit.timeit(net.forward, number=1)
