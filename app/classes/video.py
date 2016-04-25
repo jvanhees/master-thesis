5953dbfed2efc31a228af29d8b3c8873f782aa8e
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-04-25 11:10:24 +0200

    Video test

--- app/classes/video.py
+++ app/classes/video.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import math
+from skimage import img_as_float
 
 frames = 25
 
@@ -12,14 +14,40 @@ class Video:
         self.frames = self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
         self.width = self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
         self.height = self.cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
-
+        
+        success, frame = self.cap.read()
+        if success:
+            self.channels = frame.shape[2]
+        else:
+            return false
+        
+    # Returns a single frame that is prepared for caffe
     def getFrame(self, frameNumber):
         self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, frameNumber)
         success, frame = self.cap.read()
         if success:
-            return self.convertToRGB(frame)
+            return self.prepareForCaffe(frame)
         else:
             return False
+    
+    # Returns an array of frames prepared for use in Caffe
+    def getFrames(self, interval, start=None):
+        if start is None:
+            start = 0
+        
+        # Calculate number of frames that will be returned
+        it = int(math.floor(self.frames / interval))
+        frames = np.zeros((it, self.height, self.width, self.channels))
+        
+        for i in range(it):
+            self.cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, i * interval)
+            success, frame = self.cap.read()
+            
+            if success:
+                frames[i] = self.prepareForCaffe(frame)
+        
+        return frames
+        
             
     def showFrame(self, frame):
         cv2.imshow('Frame',frame)
@@ -28,5 +56,5 @@ class Video:
         # Release everything if job is finished
         self.cap.release()
     
-    def convertToRGB(self, frame):
-        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    def prepareForCaffe(self, frame):
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB).astype('float') / 255
