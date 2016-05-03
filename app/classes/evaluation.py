3075140cb87a769c33ba238e750fcf38089d3e20
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-05-03 17:31:22 +0200

    Evaluation class

---
+++ app/classes/evaluation.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import CaffeNet
+from data import Clip
+
+
+frames = 25
+
+class Evaluate:
+    
+    def __init__(self, mediaclip):
+        # Get mediaclip thumbnail image
+        
+        # Get concepts for mediaclip thumbnail image with caffenet
+        
+    
+    
+    def __del__(self):
+        # Desctruction method
+
+    
+    # Returns True / False
+    # Accepts frame image, concepts from CaffeNet
+    def frame(self, frame, concepts):
+        # Get concepts for frame with caffenet
+        
+        # Compare mediaclip thumbnail concepts with frame concepts
+        
+        # Decide True / False
