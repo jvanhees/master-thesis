632c82c745b5c92c1a9248cdd28cbc5689350d7d
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-06-21 00:40:07 +0200

    CaffeNet cleanup

--- app/classes/caffenet.py
+++ app/classes/caffenet.py
@@ -4,39 +4,42 @@ import numpy as np
 import caffe
 
 class CaffeNet:
-    
+        
     def __init__(self):
-        caffe_root = '/Users/JVH/Development/caffe/'
         # RGB mode
         self.channels = 3
+        
         # Load model
-        if os.path.isfile(caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
-            print 'CaffeNet found.'
-        else:
-            print 'CaffeNet not found.'
-            
-        caffe_root = '/Users/JVH/Development/caffe/'
+        self.caffe_root = '/Users/JVH/Development/caffe/'
+        if not os.path.isfile(self.caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'):
+            raise CaffeError('CaffeNet not found')
 
         # Load labels
-        labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt'
+        labels_file = self.caffe_root + 'data/ilsvrc12/synset_words.txt'
         if not os.path.exists(labels_file):
             print 'Labels not found.'
 
         self.labels = np.loadtxt(labels_file, str, delimiter='\t')
-
-        model_def = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
-        model_weights = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+        
+        self.loaded = False
+    
+    
+    def load(self):
+        print 'Initializing CaffeNet'
+        model_def = self.caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
+        model_weights = self.caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
 
         caffe.set_device(0)  # if we have multiple GPUs, pick the first one
         caffe.set_mode_gpu()
 
         # Start net
         self.net = caffe.Classifier(model_def, model_weights,
-                               mean=np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
+                               mean=np.load(self.caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
                                channel_swap=(2,1,0),
                                raw_scale=255,
                                image_dims=(256, 256))
-                               
+        self.loaded = True
+    
                                
     def getLabels(self):
         return self.labels
@@ -45,6 +48,9 @@ class CaffeNet:
         return caffe.io.load_image(image)
     
     def classify(self, frames):
+        if not self.loaded:
+            self.load()
+        
         return self.net.predict(frames)
     
     def getTopConcepts(self, output, count=None):
