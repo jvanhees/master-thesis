c72920fde72747ddffddf9d683b5345219e4e277
Jorick van Hees <j.vanhees@bluebillywig.com>
2016-06-24 20:43:00 +0200

    Included other news articles in survey

--- survey/inc/card.php
+++ survey/inc/card.php
@@ -1,22 +1,27 @@
 <?php
 if($item == 1){
+	$clip = '2485205';
 	$videoId = '2651860';
 	$title = 'Sea Princess';
-	$text = 'Visitors from Australia and New Zealand arrive in Cobh on board the cruise liner Sea Princess on their world tour from Sydney.
-	Video Eddie O\'Hare Evening Echo';
+	$text = 'Visitors from Australia and New Zealand arrive in Cobh on board the cruise liner Sea Princess on their world tour from Sydney.';
 } elseif($item == 2){
-	
+	$clip = '2452879';
+	$videoId = '2652964';
+	$title = 'Motors and Me Patrick O\'Sullivan';
+	$text = 'Patrick O\'Sullivan from Clonakilty with his 1968 Daimler 420 Sovereign.';
 } elseif($item == 3){
-	
+	$clip = '2489018';
+	$videoId = '2652966';
+	$title = 'Roller Derby double header hosted at Little Island Sports Complex';
+	$text = 'Mars Grand Dam O\'Reilly, Co Captain of Cork City Firebirds at the Roller Derbydouble header hosted at Little Island Sports Complex.';
 }
 
 if($page == 'without'){
 	?>
 	<article class="article-preview card">
 		<div class="card-content">
-			<span class="card-title">Sea Princess</span>
-			<p>Visitors from Australia and New Zealand arrive in Cobh on board the cruise liner Sea Princess on their world tour from Sydney.
-	Video Eddie O'Hare Evening Echo</p>
+			<span class="card-title"><?=$title?></span>
+			<p><?=$text?></p>
 		</div>
 	</article>
 	<?php
